7019ef052e48c8deb7fe233b93d4beae427ddd73
michaelangelesz <125311967+michaelangelesz@users.noreply.github.com>
2023-08-14 21:11:14 -0400

    add suspenseful spinner

--- src/App.js
+++ src/App.js
@@ -1,36 +1,45 @@
-import {useState, useEffect} from 'react'
-import Gallery from './components/Gallery';
-import SearchBar from './components/SearchBar';
+import './App.css';
+import { useState, Suspense, useEffect } from 'react'
+import Gallery from './components/Gallery'
+import SearchBar from './components/SearchBar'
+import Spinner from './components/Spinner'
+import { createResource as fetchData } from './helper'
+
 function App() {
-  let [message, setMessage] = useState('Seach for music')
-  let [search, setSearch] = useState('')
-  let [data, setData] = useState([])
-  const API_URL = 'https://itunes.apple.com/search?term='
-  useEffect(()=>{
-    if(search){
-    const fetchData = async () => {
-      document.title = `${search} music`
-      const response = await fetch(API_URL + search)
-      const resData = await response.json()
-      if(resData.results.length > 0){
-        setData(resData.results)
-      }else{
-        setMessage('Not Found')
-      }
-    }
-    fetchData()
-    }
-  }, [search])
-  const handleSearch = (e, term) => {
-    e.preventDefault();
-    setSearch(term)
+  let [searchTerm, setSearchTerm] = useState('')
+  let [message, setMessage] = useState('Search for Music!')
+  let [data, setData] = useState(null)
+
+  useEffect(() => {
+    if (searchTerm) {
+      document.title=`${searchTerm} Music`
+      console.log(fetchData(searchTerm))
+      setData(fetchData(searchTerm))
   }
+  }, [searchTerm])
+
+  const handleSearch = (e, term) => {
+    e.preventDefault()
+    setSearchTerm(term)
+  }
+
+  const renderGallery = () => {
+    if(data){
+      return (
+        <Suspense fallback={<Spinner />}>
+          <Gallery data={data} />
+        </Suspense>
+      )
+    }
+  }
+
   return (
     <div className="App">
-      <SearchBar handleSearch={handleSearch}/>
+      <SearchBar handleSearch={handleSearch} />
       {message}
-      <Gallery data={data}/>
+      {renderGallery()}
     </div>
   );
 }
+
 export default App;
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,13 +1,16 @@
-import { useState } from 'react'
+import { useState } from "react"
 
-function SearchBar(props) {
-    let [searchTerm, setSearchTerm] = useState('')
+const SearchBar = (props) => {
+  let [searchTerm, setSearchTerm] = useState("")
+
   return (
     <form onSubmit={(e) => props.handleSearch(e, searchTerm)}>
-        <input type="text" placeholder="search here"
+      <input
+        type="text"
+        placeholder="Search Here"
         onChange={(e) => setSearchTerm(e.target.value)}
-        />
-        <input type="submit"/>
+      />
+      <input type="submit" />
     </form>
   )
 }
